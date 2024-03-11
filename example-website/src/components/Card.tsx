31baa61e225f5b0839376e097ee6fdca1e024a37
John <codecreator127@gmail.com>
2024-03-11 21:05:58 +1300

    card and card container components made, hard coded content for now

--- example-website/src/components/Card.tsx
+++ example-website/src/components/Card.tsx
@@ -9,15 +9,15 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, description, imgSrc, imgAlt }) => {
   return (
-    <div className="card card-compact bg-base-100 shadow-xl">
+    <div className="card w-96 bg-base-100 shadow-xl image-full">
       <figure>
-        <img src={imgSrc} alt={imgAlt} className="h-48 w-full object-cover" />
+        <img src={imgSrc} alt={imgAlt} className="w-full" />
       </figure>
-      <div className="card-body">
+      <div className="card-body opacity-0 hover:opacity-100 transition-opacity duration-300">
         <h2 className="card-title">{title}</h2>
         <p>{description}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Learn More</button>
+          <button className="btn btn-primary">Check It Out!</button>
         </div>
       </div>
     </div>
--- example-website/src/components/CardContainer.tsx
+++ example-website/src/components/CardContainer.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import Card from './Card';
 
+import Image from './test_image.jpeg';
+
 const CardContainer = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
       <Card
-        title="Card Title 1"
-        description="This is the description for Card 1."
-        imgSrc="/path/to/image1.jpg"
-        imgAlt="Card 1 Image"
+        title="Project 1"
+        description="Project description, what we did, how we did it. Who the client was etc."
+        imgSrc={Image.src}
+        imgAlt="Project 1"
       />
       <Card
-        title="Card Title 2"
-        description="This is the description for Card 2."
-        imgSrc="/path/to/image2.jpg"
-        imgAlt="Card 2 Image"
+        title="Project 2"
+        description="Project description, what we did, how we did it. Who the client was etc."
+        imgSrc={Image.src}
+        imgAlt="Project 2"
       />
       <Card
-        title="Card Title 3"
-        description="This is the description for Card 3."
-        imgSrc="/path/to/image3.jpg"
-        imgAlt="Card 3 Image"
+        title="Project 3"
+        description="Project description, what we did, how we did it. Who the client was etc."
+        imgSrc={Image.src}
+        imgAlt="Project 3"
       />
     </div>
   );
