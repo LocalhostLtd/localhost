35e32d0486500e506fc5b9b71a6a21cdee00c5af
John <codecreator127@gmail.com>
2024-03-13 22:36:17 +1300

    testing images

--- example-website/src/components/Card.tsx
+++ example-website/src/components/Card.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
+import Link from 'next/link';
 
 interface CardProps {
   title: string;
   description: string;
   imgSrc: string;
   imgAlt: string;
+  linkUrl: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, description, imgSrc, imgAlt }) => {
+const Card: React.FC<CardProps> = ({ title, description, imgSrc, imgAlt, linkUrl}) => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl image-full">
-      <figure>
-        <img src={imgSrc} alt={imgAlt} className="w-full" />
-      </figure>
-      <div className="card-body opacity-0 hover:opacity-100 transition-opacity duration-300">
-        <h2 className="card-title">{title}</h2>
-        <p>{description}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Check It Out!</button>
+    <Link href={linkUrl} target="_blank" rel="noopener noreferrer" passHref>
+
+        <div className="card w-96 bg-base-100 shadow-xl image-full">
+            <figure>
+                <img src={imgSrc} alt={imgAlt} className="w-full" />
+            </figure>
+            <div className="card-body opacity-0 hover:opacity-100 transition-opacity duration-300">
+                <h2 className="card-title">{title}</h2>
+                <p>{description}</p>
+                <div className="card-actions justify-end">
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- example-website/src/components/CardContainer.tsx
+++ example-website/src/components/CardContainer.tsx
@@ -2,27 +2,31 @@ import React from 'react';
 import Card from './Card';
 
 import Image from './test_image.jpeg';
+import XIVImage from '../../public/XIV.png'
 
 const CardContainer = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
       <Card
-        title="Project 1"
+        title="XIV"
         description="Project description, what we did, how we did it. Who the client was etc."
-        imgSrc={Image.src}
+        imgSrc={XIVImage.src}
         imgAlt="Project 1"
+        linkUrl='https://tonylxm.github.io/sesa-x-wdcc-hackathon/'
       />
       <Card
         title="Project 2"
         description="Project description, what we did, how we did it. Who the client was etc."
         imgSrc={Image.src}
         imgAlt="Project 2"
+        linkUrl='https://www.google.com/'
       />
       <Card
         title="Project 3"
         description="Project description, what we did, how we did it. Who the client was etc."
         imgSrc={Image.src}
         imgAlt="Project 3"
+        linkUrl='https://www.google.com/'
       />
     </div>
   );
