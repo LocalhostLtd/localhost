1f8bdb0296dc3e70db1c166e6549c5e538ad0aac
John <codecreator127@gmail.com>
2024-03-14 22:31:26 +1300

    rounded header, made it half transparent too

--- example-website/src/components/header.tsx
+++ example-website/src/components/header.tsx
@@ -1,12 +1,12 @@
 function Header(): any {
   return (
-    <div className="navbar bg-base-100 rounded">
+    <div className="navbar bg-base-100 rounded-3xl sticky top-0 z-10 px-5 bg-opacity-50">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">Example Website</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li><a>About Us</a></li>
+          <li><a>about us</a></li>
         </ul>
       </div>
     </div>
