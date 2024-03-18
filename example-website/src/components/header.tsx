93376ca2dafded506791f34e20ede5b2095a8bba
John <codecreator127@gmail.com>
2024-03-18 20:26:14 +1300

    added burger menu icon, and sizing

--- example-website/src/components/header.tsx
+++ example-website/src/components/header.tsx
@@ -1,13 +1,20 @@
 function Header(): any {
   return (
-    <div className="navbar bg-base-100 rounded-3xl sticky top-0 z-10 px-5 bg-opacity-50">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Example Website</a>
+    <div className="p-5">
+      <div className="navbar bg-base-100 rounded-3xl sticky top-0 z-10 px-10 bg-opacity-60">
+      <div className="flex-none lg:hidden md:hidden sm:block sm:px-2">
+        <button className="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </button>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>about us</a></li>
-        </ul>
+        <div className="flex-1">
+          <a href="" className="text-xl">localhost</a>
+        </div>
+        <div className="flex-none hidden lg:block md:block">
+          <ul className="menu menu-horizontal px-1">
+            <li><a>about us</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   )
