276a5bfa53de0b86fb807698c7d52f32740f3431
John <codecreator127@gmail.com>
2024-03-24 12:51:35 +1300

    bug fix on page.tsx

--- example-website/src/app/page.tsx
+++ example-website/src/app/page.tsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
-import Header from '@/components/header';
+import Image from "../../public/XIV.png";
 import CardContainer from "@/components/CardContainer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div>
-        <Header />
         <CardContainer />
       </div>
-    <main/>
+    </main>
   );
 }
