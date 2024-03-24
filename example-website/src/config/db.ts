848f388cb0ca9ccd9a61487ee9f0d50a382e6b4d
Oculux <nate.williamson.nz@gmail.com>
2024-03-24 18:12:27 +1300

    :bricks: set up dotenv

--- example-website/src/config/db.ts
+++ example-website/src/config/db.ts
@@ -1,42 +1,12 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import { MONGO_URL } from "./env";
 
-// MongoDB connection URI
-const mongoURI = "mongodb://localhost:27017/mydatabase";
-
-// Connect to MongoDB
 mongoose
-  .connect(mongoURI)
+  .connect(MONGO_URL)
   .then(() => {
     console.log("Successfully connected to MongoDB.");
   })
   .catch((err: Error) => {
     console.error("Error connecting to MongoDB: ", err);
+    process.exit(1);
   });
-
-/*
-// Define a schema
-const Schema = mongoose.Schema;
-const exampleSchema = new Schema({
-  // Define your schema fields here
-  name: String,
-  age: Number,
-});
-
-// Define a model
-const ExampleModel = mongoose.model("Example", exampleSchema);
-
-// Example usage: Create a new document
-const exampleDoc = new ExampleModel({
-  name: "John",
-  age: 30,
-});
-
-exampleDoc
-  .save()
-  .then((doc: any) => {
-    console.log("Document saved:", doc);
-  })
-  .catch((err: any) => {
-    console.error("Error saving document:", err);
-  });
-*/
