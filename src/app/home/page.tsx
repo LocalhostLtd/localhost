3cf392a096f126b685b4f67859db210dc5088a1d
John <codecreator127@gmail.com>
2024-03-29 17:07:53 +1300

    added code to the home page, added package-lock to gitignore

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/header';
+import CardContainer from "@/components/CardContainer";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <div>
+        <CardContainer />
+      </div>
+    </main>
+  );
+}
