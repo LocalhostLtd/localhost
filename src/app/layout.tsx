fe2adb9a4782783307fd034cf087c33459e61b99
dis-may <maygan275@gmail.com>
2024-03-30 18:58:35 +1300

    Added components to page.tsx to avoid errors and fix header sizing problems

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import Footer from "../components/footer";
+import "./globals.css";6
 import "../config/db";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <Footer />
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Header from '@/components/header';
+import Footer from '@/components/footer';
 import CardContainer from "@/components/CardContainer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main >
       <Header />
-      <div>
+      <div className="flex min-h-screen flex-col items-center justify-between">
         <CardContainer />
       </div>
+      <Footer />
     </main>
   );
 }
