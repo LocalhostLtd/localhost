b59c5b3b1c62adc0d64d08f12c8d55959c40e931
dis-may <maygan275@gmail.com>
2024-04-07 01:28:15 +1300

    home page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
+import Heading from "@/components/Heading";
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import CardContainer from "@/components/CardContainer";
-import HomeBackground from '../../public/home_background.PNG';
+import MainText from "@/components/MainText";
+import HomeBackground from '../../public/homeBackgroundGreen.png';
+
+// below are possible ways to add the coloured overlay using css (which would make it easy to change themes)
+// currently the green background is part odf the image because text appears behind the coloured overlay otherwise.
+// , boxShadow: "inset 0 0 0 1000px rgba(0,55,0,.2)"
+// <div className="fixed top-0 left-0 w-48 h-full z-0 bg-base-100 z-0 opacity-80"></div>
 
 export default function Home() {
+  const mainText = "Need a website? Leave it to us.";
+  const subText = "We're a group of developers";
   return (
-    <div className="bg-cover bg-center bg-customGreen bg-opacity-10" style={{backgroundImage: `url(${HomeBackground.src})`}}>  
-      <div className="fixed top-0 left-0 w-full h-full  bg-customGreen opacity-70"></div>
+    <div className="bg-cover bg-center" style={{backgroundImage: `url(${HomeBackground.src})`}}>
+      
       <Header />
-      <h1 className="font-sans text-white">Need a website? Leave it to us</h1>
+      <div className="m-[max(15vw,20px)] flex-1 items-center justify-center">
+        <h1 className="font-sans text-accent text-5xl text-center drop-shadow-lg">Need a website? Leave it to us.</h1>
+        <p className="font-sans text-base-content font-bold text-2xl text-center m-10 drop-shadow-lg">
+          We’re a group of university students united on our common interest in web design and development.
+        </p>
+      </div>
+      <Heading title="our projects" />
       <div className="flex min-h-screen flex-col items-center justify-between">
-        <CardContainer />
+        <CardContainer /> 
       </div>
       <Footer />
-    </div>
+      </div>
   );
 }
